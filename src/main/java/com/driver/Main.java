aca22dca1af88237411a9459cbc12d61ddb85c82
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-21 12:26:21 +0530

    commited

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,4 +6,18 @@ public class Main {
         B ob_b = new B();
         System.out.print(ob_b.meth());
     }
+    public static class A {
+        public String meth()
+        {
+            return "Invoking method from class A";
+        }
+
+    }
+    public static class B extends A{
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+
+        }
+    }
 }
